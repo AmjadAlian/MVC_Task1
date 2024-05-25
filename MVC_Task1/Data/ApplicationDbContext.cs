8d3032e5f2a3190f1290cde1475d074d899ee1de
amjad alian <amjad@amjadad.onmicrosoft.com>
2024-05-25 17:47:04 +0300

    Add project files.

---
+++ MVC_Task1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Task1.Models;
+
+namespace MVC_Task1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Employee> employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;database=MVC_Task1;trusted_connection=true;TrustServerCertificate=true");
+        }
+
+    }
+}
---
+++ MVC_Task1/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Task1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string title { get; set; } = null!;
+
+        [Required]
+        public int Age { get; set; }
+    }
+}
